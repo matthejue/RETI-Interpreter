5e1765d4e848634cf00e6ac331328b9f5c706523
areotwister <areotwister@gmail.com>
2022-04-08 03:25:28 +0200

    almost finished interpreter

--- src/interp_reti.py
+++ src/interp_reti.py
@@ -3,20 +3,30 @@ from reti import RETI
 
 
 class Interp_RETI:
-    def interp_memory(self, arg, reti, deref=0):
+    def interp_memory(self, arg, reti) -> int:
         match arg:
             case NT.Immediate(val):
+                higher_bits = (reti.registers["DS"] >> 22) % 0b100000000 << 22
                 memory_type = reti.registers["DS"] >> 30
                 if memory_type == 0b00:
-                    return reti.eprom[val + deref]
+                    return reti.eprom[val + higher_bits]
                 elif memory_type == 0b01:
-                    return reti.uart[val + deref]
+                    return reti.uart[val + higher_bits]
                 else:
-                    return reti.sram[val + deref]
+                    return reti.sram[val + higher_bits]
             case NT.Reg(reg):
                 return reti.registers[reg]
-            # immediate is always a memory address here, because all
-            # 'immediate' instructions are always unambiguous
+            case int():
+                memory_type = arg >> 30
+                if memory_type == 0b00:
+                    return reti.eprom[arg]
+                elif memory_type == 0b01:
+                    return reti.uart[arg]
+                else:
+                    return reti.sram[arg]
+            case _:
+                #  raise TODO: sich hier was überlegen
+                ...
 
     def interp_instruction(self, instr, reti):
         match instr:
@@ -67,20 +77,31 @@ class Interp_RETI:
                     arg, reti
                 )
             case NT.Instr(NT.Andi(), NT.Reg(reg), NT.Immediate(val)):
+                # TODO: wegen Signextension überlegen
                 reti.registers[reg] = reti.registers[reg] & int(val)
-            case NT.Instr(NT.Load(), NT.Reg(reg), arg):  # arg=M(i)
-                reti.registers[reg] = self.interp_memory(arg, reti)
-            #  case NT.Instr(NT.Loadin(), reg_base, NT.Reg(reg), deref):
-            #      reti.registers[reg] = self.interp_memory(reg_base, reti, self.interp_memory(deref, reti))
-            # TODO: continue here
-            #  case NT.Instr(NT.Loadi(), NT.Reg(reg), NT.Immediate(val)):
-            #  case NT.Instr(NT.Store(), NT.Reg(reg), NT.Immediate(val)):
-            #  case NT.Instr(NT.Storein(), NT.Reg(reg), NT.Immediate(val)):
-            #  case NT.Instr(NT.Move(), NT.Reg(reg), NT.Immediate(val)):
+            case NT.Instr(NT.Load(), NT.Reg(reg), NT.Immediate() as M):  # arg=M(i)
+                reti.registers[reg] = self.interp_memory(M, reti)
+            case NT.Instr(
+                NT.Loadin(), NT.Reg() as reg_base, NT.Reg(reg), NT.Immediate(deref)
+            ):  # reg_base=Reg
+                reti.registers[reg] = self.interp_memory(
+                    self.interp_memory(reg_base, reti) + int(deref), reti
+                )
+            case NT.Instr(NT.Loadi(), NT.Reg(reg), NT.Immediate(val)):
+                reti.registers[reg] = (
+                    int(val) + (reti.registers["DS"] >> 22) % 0b100000000 << 22
+                )
+            case NT.Instr(NT.Store(), NT.Reg(reg), NT.Immediate(val) as M):
+                #  reti. = reti.registers[reg]
+                ...
+            case NT.Instr(NT.Storein(), NT.Reg(reg1), NT.Reg(reg2), NT.Immediate(val)):
+                #  reti = reti.registers[reg]
+                ...
+            case NT.Instr(NT.Move(), NT.Reg(reg1), NT.Reg(reg2)):
+                reti.registers[reg2] = reti.registers[reg1]
             #  case NT.Jump(NT.Reg(reg), NT.Immediate(val)):
             #  case NT.Int(NT.Reg(reg), NT.Immediate(val)):
             #  case NT.Rti(NT.Reg(reg), NT.Immediate(val)):
-        return reti
 
     def interp_program(self, p):
         p.update_match_args()
