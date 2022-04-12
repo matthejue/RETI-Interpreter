b0f492f9fbf71a5d45b387817c8bb365788d65a8
areotwister <areotwister@gmail.com>
2022-04-12 03:35:49 +0200

    small correction

--- extract_input_and_expected.sh
+++ extract_input_and_expected.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for test in ./tests/*.reti; do
-  sed -n '1p' $test | sed -e 's/^# in://' > ./${test%.reti}.in
-  sed -n '2p' $test | sed -e 's/^# expected://' > ./${test%.reti}.out_expected
+  sed -n '1p' $test | sed -e 's/^# in://' > ${test%.reti}.in
+  sed -n '2p' $test | sed -e 's/^# expected://' > ${test%.reti}.out_expected
 done
